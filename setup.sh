7dfb6f04a847322d8bdd0e308cd36fbed022e76e
bitcrusherr <133945128+bitcrusherr@users.noreply.github.com>
2023-08-29 12:20:20 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,13 +32,15 @@ while [ "$Y_N" = "y" ]; do
 prometheus="y"
 echo "installing Promethues"
 sudo wget https://github.com/prometheus/prometheus/releases/download/v2.44.0/prometheus-2.44.0.linux-amd64.tar.gz --directory-prefix "/home/$logname"
-sudo tar xvfz /home/$logname/prometheus-2.44.0.linux-amd64.tar.gz
+sudo tar xvfz /home/$logname/prometheus-2.44.0.linux-amd64.tar.gz -C /home/$logname/prometheus
 sudo rm prometheus-2.44.0.linux-amd64.tar.gz
 echo
 echo "Creating system service"
 echo
 sleep 1s
-sudo printf "[Unit]\nDescription=prometheus\n\n[Service]\nExecStart=/home/$logname/prometheus-2.44.0.linux-amd64\prometheusn\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/prometheus.service
+sudo printf "[Unit]\nDescription=prometheus\n\n[Service]\nExecStart=/home/$logname/prometheus\prometheus\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus.service
 Y_N=n
 done
 
